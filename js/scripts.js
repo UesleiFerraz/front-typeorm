f2ccf15911f4310aa5baa7216be96705b056e397
UesleiFerraz <uesleivaleriod@gmail.com>
2021-07-08 22:50:12 -0300

    small changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,18 @@
 axios.defaults.baseURL = "http://localhost:3000";
 
+const token = localStorage.getItem("token");
+const button = document.querySelector("#save");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const repeatPassword = document.querySelector("#repeat-password");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const tBody = document.querySelector("tbody");
 
-async function registerUser(e) {
-  e.preventDefault();
-
-  // !username.value ? (usernameError.innerHTML = "username invalido") : "";
-
-  // if (password.value.length < 6) {
-  //   passwordError.innerHTML = "A senha deve ter pelo menos 6 caracteres";
-  // }
-
-  // if (password.value !== repeatPassword.value) {
-  //   repeatPasswordError.innerHTML = "As senhas não são iguais";
-  // }
-
+if (document.querySelector("#signUp")) {
+}
+async function registerUser(event) {
+  event.preventDefault();
   if (
     username.value !== "" &&
     password.value.length >= 6 &&
@@ -37,8 +33,11 @@ async function registerUser(e) {
   }
 }
 
-async function login(e) {
-  e.preventDefault();
+if (document.querySelector("#signIn")) {
+}
+
+async function login(event) {
+  event.preventDefault();
   try {
     const user = await axios.post("/auth", {
       username: username.value,
@@ -56,4 +55,123 @@ async function login(e) {
       iconShowHidePassword1.classList.add("invalid");
     }
   }
+}
+
+if (document.querySelector("#scraps")) {
+  if (token) {
+    try {
+      JSON.parse(atob(token.split(".")[1])).userIdToken;
+    } catch {
+      location = "index.html?error=login";
+    }
+  } else {
+    location = "index.html?error=login";
+  }
+
+  (async function getScraps() {
+    const response = await axios.get(`/scraps`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const scraps = response.data.scraps;
+
+    console.log("oi");
+
+    scraps.reverse();
+    scraps.forEach(scrap => {
+      tBody.innerHTML += `
+      <tr id="id_${scrap.id}">
+      ${createElement(scrap).innerHTML}
+      </tr>
+      `;
+    });
+  })();
+}
+
+async function saveScrap(event) {
+  event.preventDefault();
+
+  if (button.id) {
+    editScrap();
+  } else {
+    const response = await axios.post(
+      "/scraps",
+      {
+        title: title.value,
+        description: description.value,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    const scrap = response.data.scrap;
+    tBody.prepend(createElement(scrap));
+    title.value = "";
+    description.value = "";
+  }
+}
+
+async function editScrap() {
+  const response = await axios.put(
+    `/scraps/${button.id}`,
+    {
+      title: title.value,
+      description: description.value,
+    },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  const scrap = await response.data.scrap;
+
+  document.querySelector(`#id_${button.id}`).innerHTML =
+    createElement(scrap).innerHTML;
+
+  title.value = "";
+  description.value = "";
+  button.id = "";
+}
+
+function signOff() {
+  localStorage.removeItem("token");
+  location = "index.html";
+}
+
+function deleteTodo(id) {
+  new bootstrap.Modal(document.getElementById("myModal"), {}).show();
+
+  document.querySelector("#excluir").addEventListener("click", async event => {
+    event.preventDefault;
+    document.querySelector(`#id_${id}`).remove();
+
+    await axios.delete(`/scraps/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  });
+}
+
+function editTodo(id) {
+  const data = document.querySelector(`#id_${id}`);
+
+  title.value = data.querySelectorAll("td")[1].innerText;
+  description.value = data.querySelectorAll("td")[2].innerText;
+
+  button.id = id;
+}
+
+function createElement(scrap) {
+  let trDOM = document.createElement("tr");
+  trDOM.id = `id_${scrap.id}`;
+  trDOM.innerHTML = `
+      <td scope="row">ueslei</td>
+      <td>${scrap.title}</td>
+      <td>${scrap.description}</td>
+      <td>
+      <a onclick="editTodo('${scrap.id}')">Editar</a>
+      <a onclick="deleteTodo('${scrap.id}')">Excluir</a>
+      </td>
+      `;
+
+  return trDOM;
 }
